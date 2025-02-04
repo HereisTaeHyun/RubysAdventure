09a604e32d4dd583b3e99c75105eedec826ad846
HereisTaeHyun <kth882500@gmail.com>
2025-02-04 15:50:43 +0900

    MovePlus

--- Assets/2. Scripts/RubyController.cs
+++ Assets/2. Scripts/RubyController.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float moveSpeed = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // QualitySettings.vSyncCount = 0;
+        // Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        // Debug.Log(horizontal);
         Vector2 position = transform.position;
-        position.x += 0.1f;
+        position.x += moveSpeed * horizontal * Time.deltaTime;
+        position.y += moveSpeed * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
