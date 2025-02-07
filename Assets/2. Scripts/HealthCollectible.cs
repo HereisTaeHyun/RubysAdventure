1881e3c54624c0d1313fd4339bb0e1d1487936f1
HereisTaeHyun <kth882500@gmail.com>
2025-02-07 15:41:41 +0900

    healthItemPlus

---
+++ Assets/2. Scripts/HealthCollectible.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        RubyController rubyController = other.GetComponent<RubyController>();
+        if(rubyController != null)
+        {
+            if(rubyController.health < rubyController.maxHealth)
+            {
+                rubyController.ChangeHealth(1);
+                Destroy(gameObject); 
+            }
+        }
+    }
+}
--- Assets/2. Scripts/RubyController.cs
+++ Assets/2. Scripts/RubyController.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    Rigidbody2D rigidbody2D;
+    Rigidbody2D rb2D;
     public float moveSpeed = 50.0f;
+    public int maxHealth = 5;
+    public int health{get{return currentHealth;}}
+    private int currentHealth;
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+        // currentHealth = maxHealth;
+        currentHealth = 1;
     }
 
     // Update is called once per frame
@@ -18,9 +24,15 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector2 position = rigidbody2D.position;
+        Vector2 position = rb2D.position;
         position.x += moveSpeed * horizontal * Time.deltaTime;
         position.y += moveSpeed * vertical * Time.deltaTime;
-        rigidbody2D.MovePosition(position);
+        rb2D.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth); // currentHealth + amount = 제한 값으로 이 값이 0 ~ maxHealth에 있게됨
+        Debug.Log($"{currentHealth} / {maxHealth}");
     }
 }
